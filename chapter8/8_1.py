d6ef8d69c6c96660cf0e334f3c2f9aea2a06b06c
Mike <mike_deen@bk.ru>
2023-07-22 21:02:59 +0300

    Exercise 8_1 is added.

---
+++ chapter8/8_1.py
@@ -0,0 +1,13 @@
+a = 'qwerty'
+# print(a.strip('q'))
+# print(a.replace('y', a))
+def s_replace(a, y):
+    if len(a) > 100:
+        return a
+    else:
+        a = a.replace(y, a)
+        #print(a)
+        return s_replace(a, y)
+
+# print(s_replace('qwerty', 'y'))
+
