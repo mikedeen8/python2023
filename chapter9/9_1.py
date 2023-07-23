1ca8c68f3bff72cdcc7953854b52a98ae349d5c8
Mike <mike_deen@bk.ru>
2023-07-23 17:04:29 +0300

    Exercise 9_1 is added.

--- chapter9/9_1.py
+++ chapter9/9_1.py
@@ -1,6 +1,7 @@
-fin = open('python2023/chapter9/words_folder/words.txt')
-line = fin.readline()
-for line in fin:
-    word = line.strip()
-    if len(word) > 20:
-        print(word)
+def words():
+    fin = open('python2023/chapter9/words_folder/words.txt')
+    line = fin.readline()
+    for line in fin:
+        word = line.strip()
+        if len(word) > 20:
+            print(word)
