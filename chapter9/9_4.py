6674188088a22fa2336f8f5bde43732ba8ca4b68
Mike <mike_deen@bk.ru>
2023-07-29 13:43:03 +0300

    Exercise 9_4 is fixed.

--- chapter9/9_4.py
+++ chapter9/9_4.py
@@ -1,15 +1,10 @@
 def uses_only(word, letters):
-    for l in letters:
+    for l in word:
         if l == ' ' or l == '.' or l == ','or l == '?' or l == '!':
             pass
-        elif not l.upper() in word and not l.lower() in word:
-            return False
-    for c in word:
-        if c == ' ' or c == '.' or c == ',' or c == '?' or c == '!':
-            pass
-        elif not c.upper() in letters and not c.lower() in letters:
+        elif not l.upper() in letters and not l.lower() in letters:
             return False
     return True
 
-print(uses_only('wasdwvc', 'wasdvc'))
+print(uses_only('wasdwd', 'wasdvcf'))
 # print(uses_only('Hello, fecal hole!', 'acefhlo'))         # :)
