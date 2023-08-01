aea22db35979d6f2027e28600aa0a2969394421c
Mike <mike_deen@bk.ru>
2023-08-01 21:01:46 +0300

    Exercise 10_2 is fixed.

--- chapter10/10_2.py
+++ chapter10/10_2.py
@@ -1,10 +1,11 @@
 def cumsum(t):
+    c = t
     for i in range(len(t)):
         if i == 0:
             pass
         else:
-            t[i] += t[i - 1]
+            c[i] += t[i - 1]
             i += 1
-    return t
+    return c
 
 print(cumsum([1, 2, 3]))
